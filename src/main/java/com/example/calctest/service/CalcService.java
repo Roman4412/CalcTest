257fb5eba2c5267473e0f4af3e57bcd7cc55fd2a
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-05-12 05:58:58 +0300

    initial

---
+++ src/main/java/com/example/calctest/service/CalcService.java
@@ -0,0 +1,34 @@
+package com.example.calctest.service;
+
+
+import com.example.calctest.CalcInterface.CalcInterface;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class CalcService implements CalcInterface {
+    @Override
+    public String toGreet() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public int plus(int num1, int num2) {
+        return num1+num2;
+    }
+
+    @Override
+    public int minus(int num1, int num2) {
+        return num1 - num2;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        return num1 * num2;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        return num1 / num2;
+    }
+}
