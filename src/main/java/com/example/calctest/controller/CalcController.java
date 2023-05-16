d1ecc1bd58b7c77f253a38dac4b3095273904eeb
RomanPustovalov <120693916+RomanPustovalov@users.noreply.github.com>
2023-05-16 03:39:20 +0300

    controller fix

--- src/main/java/com/example/calctest/controller/CalcController.java
+++ src/main/java/com/example/calctest/controller/CalcController.java
@@ -1,7 +1,6 @@
 package com.example.calctest.controller;
 
 import com.example.calctest.CalcInterface.CalcInterface;
-import com.example.calctest.DefaultAdvice;
 import com.example.calctest.service.CalcService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,29 +19,17 @@ public class CalcController {
 
     @GetMapping("/plus")
     public String plus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
-        if (num2 == null || num1 == null) {
-            return "Отсутствует один из аргументов";
-        } else {
             return String.format("%d + %d = %d", num1, num2, serviceInterface.plus(num1, num2));
-        }
     }
 
     @GetMapping("/minus")
     public String minus(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
-        if (num2 == null || num1 == null) {
-            return "Отсутствует один из аргументов";
-        } else {
             return String.format("%d - %d = %d", num1, num2, serviceInterface.minus(num1, num2));
-        }
     }
 
     @GetMapping("/multiply")
     public String multiply(@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2) {
-        if (num2 == null || num1 == null) {
-            return "Отсутствует один из аргументов";
-        } else {
             return String.format("%d * %d = %d", num1, num2, serviceInterface.multiply(num1, num2));
-        }
     }
 
     @GetMapping("/divide")
